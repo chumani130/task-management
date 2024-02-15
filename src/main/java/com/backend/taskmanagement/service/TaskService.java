a8d60e3e64d246049555f6aaaa4da51f9b50133d
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-15 13:05:19 +0200

    Update TaskService.java

--- src/main/java/com/backend/taskmanagement/service/TaskService.java
+++ src/main/java/com/backend/taskmanagement/service/TaskService.java
@@ -1,4 +1,21 @@
 package com.backend.taskmanagement.service;
 
-public class TaskService {
+import com.backend.taskmanagement.model.Task;
+import com.backend.taskmanagement.model.User;
+
+import java.util.List;
+
+public interface TaskService {
+    public Task createTask(Task task, User user);
+    public Task findTaskById(Long id) throws Exception;
+
+    public void deleteTask(Long id) throws Exception;
+
+    public Task updateTask(Task task, Long id) throws Exception;
+
+    public List<Task>findAllTask();
+
+    public Task likeTask(Long taskId, User user) throws Exception;
+
+    Task likTask(Long taskId, User user) throws Exception;
 }
