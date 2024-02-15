88b3c4c5b95972a45a75842027a268f467109618
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-15 11:28:31 +0200

    Create Task.java

---
+++ src/main/java/com/backend/taskmanagement/model/Task.java
@@ -0,0 +1,24 @@
+package com.backend.taskmanagement.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String category;
+    private String priority;
+    private String description;
+    private String todolist;
+
+}
