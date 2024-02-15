8e88aa09f3fdd0256ee228b18894f87762ff4ddf
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-15 12:00:14 +0200

    Create SubTask.java

---
+++ src/main/java/com/backend/taskmanagement/model/SubTask.java
@@ -0,0 +1,20 @@
+package com.backend.taskmanagement.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class SubTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private boolean completed;
+
+}
