d748b2cf7af12994b3b0226e92fc561fb1377d6c
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-15 12:16:12 +0200

    Update Task.java

--- src/main/java/com/backend/taskmanagement/model/Task.java
+++ src/main/java/com/backend/taskmanagement/model/Task.java
@@ -1,13 +1,12 @@
 package com.backend.taskmanagement.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,12 +17,18 @@ public class Task {
     private Long id;
 
     private String title;
+    @ManyToOne
+    private User user;
     private String category;
     private String priority;
+    private String image;
     private String description;
     private String todolist;
+    private boolean completed;
     private LocalDate dueDate;
-    private LocalDate currentDate;
+    private LocalDate createdAt;
+
+    private List<Long> likes = new ArrayList<>();
 
 
 
