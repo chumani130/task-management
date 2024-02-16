0fd2f5c9945153f4e20ec4d2ab0aaad497f98627
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-16 15:17:22 +0200

    Update AppConfig.java

--- src/main/java/com/backend/taskmanagement/config/AppConfig.java
+++ src/main/java/com/backend/taskmanagement/config/AppConfig.java
@@ -1,13 +1,27 @@
 package com.backend.taskmanagement.config;
 
+import org.springframework.boot.autoconfigure.integration.IntegrationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class AppConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
+        httpSecurity.sessionManagement(management => management.sessionCreationPolicy(
+                sessionCreationPolicy.STATELESS)).authorizeHttprequests(
+                        // endpoints that should be protected
+                        Authorize => Authorize.requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()).csrf(csrf=>csrf.disabled())
+                        .cors(cors=>cors.configurationSource(CorsConfigurationSource()))
+                        .httpBasic(WithDefaults())
+                        .formLogin(WithDefaults());
         return null;
     }
+
+   
 }
