b842cad544989d43b72217b92a8c33fe8f270030
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-20 14:24:28 +0200

    Update HomeController.java

--- src/main/java/com/backend/taskmanagement/controller/HomeController.java
+++ src/main/java/com/backend/taskmanagement/controller/HomeController.java
@@ -1,12 +1,12 @@
-package com.backend.taskmanagement.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HomeController {
-    @GetMapping
-    public String homeController() {
-        return "My first Restful Api";
-    }
-}
+//package com.backend.taskmanagement.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HomeController {
+//    @GetMapping
+//    public String homeController() {
+//        return "My first Restful Api";
+//    }
+//}
