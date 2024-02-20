c0c427b702ea04743a28249704ec11a24c983526
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-20 14:24:53 +0200

    Update UserRepository.java

--- src/main/java/com/backend/taskmanagement/repository/UserRepository.java
+++ src/main/java/com/backend/taskmanagement/repository/UserRepository.java
@@ -1,9 +1,9 @@
-package com.backend.taskmanagement.repository;
-
-import com.backend.taskmanagement.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    public User findByEmail(String email);
-}
+//package com.backend.taskmanagement.repository;
+//
+//import com.backend.taskmanagement.model.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepository extends JpaRepository<User, Long> {
+//
+//    public User findByEmail(String email);
+//}
