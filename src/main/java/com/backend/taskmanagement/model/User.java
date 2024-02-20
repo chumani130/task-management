593365a30b816319e28d136ef7b70539a64f61b9
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-20 14:35:40 +0200

    Update User.java

--- src/main/java/com/backend/taskmanagement/model/User.java
+++ src/main/java/com/backend/taskmanagement/model/User.java
@@ -1,5 +1,6 @@
 package com.backend.taskmanagement.model;
 
+import com.backend.taskmanagement.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -9,9 +10,13 @@ import lombok.Data;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String password;
     private String email;
     private String userName;
+    private UserRole role;
+    @Lob
+    @Column(columnDefinition = "longblob")
+    private byte[] img;
 }
