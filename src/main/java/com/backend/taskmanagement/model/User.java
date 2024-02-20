e41a5d4b103e23037f49e043b57d09df0381a73d
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-20 14:24:44 +0200

    Update User.java

--- src/main/java/com/backend/taskmanagement/model/User.java
+++ src/main/java/com/backend/taskmanagement/model/User.java
@@ -1,15 +1,12 @@
 package com.backend.taskmanagement.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.Data;
 
-@Getter
-@Setter
+
 @Entity
+@Data
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
