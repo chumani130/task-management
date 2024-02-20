5716b9ed209d331b335dd02331336d1a9a4783c8
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-20 14:24:41 +0200

    Update Task.java

--- src/main/java/com/backend/taskmanagement/model/Task.java
+++ src/main/java/com/backend/taskmanagement/model/Task.java
@@ -1,43 +1,43 @@
-package com.backend.taskmanagement.model;
-
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-@Getter
-@Setter
-@Entity
-public class Task {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    private String title;
-
-    @ManyToOne
-    private User user;
-
-    private String category;
-
-    private String priority;
-
-    private String image;
-
-    private String description;
-    private String todolist;
-    private boolean completed;
-    private LocalDate dueDate;
-    private LocalDate createdAt;
-
-    @ElementCollection
-    private Set<Long> likes = new HashSet<>();
-
-    private int likesCount;
-
-}
+//package com.backend.taskmanagement.model;
+//
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import java.time.LocalDate;
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//import java.util.List;
+//import java.util.Set;
+//
+//@Getter
+//@Setter
+//@Entity
+//public class Task {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+//    private String title;
+//
+//    @ManyToOne
+//    private User user;
+//
+//    private String category;
+//
+//    private String priority;
+//
+//    private String image;
+//
+//    private String description;
+//    private String todolist;
+//    private boolean completed;
+//    private LocalDate dueDate;
+//    private LocalDate createdAt;
+//
+//    @ElementCollection
+//    private Set<Long> likes = new HashSet<>();
+//
+//    private int likesCount;
+//
+//}
