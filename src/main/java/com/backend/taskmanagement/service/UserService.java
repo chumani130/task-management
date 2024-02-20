0c52d261fbda630b9fc09f1419015308bbf5e46a
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-20 14:25:06 +0200

    Update UserService.java

--- src/main/java/com/backend/taskmanagement/service/UserService.java
+++ src/main/java/com/backend/taskmanagement/service/UserService.java
@@ -1,7 +1,7 @@
-package com.backend.taskmanagement.service;
-
-import com.backend.taskmanagement.model.User;
-
-public interface UserService {
-    public User findUserById(Long userId) throws Exception;
-}
+//package com.backend.taskmanagement.service;
+//
+//import com.backend.taskmanagement.model.User;
+//
+//public interface UserService {
+//    public User findUserById(Long userId) throws Exception;
+//}
