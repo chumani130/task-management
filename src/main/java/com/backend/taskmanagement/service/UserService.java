4493c7d30da46eabdea94544e98a0019d4a4fa5b
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-22 14:47:25 +0200

    Delete UserService.java

--- src/main/java/com/backend/taskmanagement/service/UserService.java
+++
@@ -1,7 +0,0 @@
-//package com.backend.taskmanagement.service;
-//
-//import com.backend.taskmanagement.model.User;
-//
-//public interface UserService {
-//    public User findUserById(Long userId) throws Exception;
-//}
