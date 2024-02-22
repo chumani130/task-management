e9b6c97fba9699243b1b1a67a96c9e4da56c86aa
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-22 15:06:39 +0200

    Create JwtRequestFilter.java

---
+++ src/main/java/com/backend/taskmanagement/filters/JwtRequestFilter.java
@@ -0,0 +1,32 @@
+package com.backend.taskmanagement.filters;
+
+import com.backend.taskmanagement.utils.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final UserDetailsServiceImpl userDetailsService
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String token = null;
+        String username = null;
+
+        if(authHeader != null && authHeader.startsWith("Bearer")) {
+            token = authHeader.substring(7);
+        }
+    }
+}
