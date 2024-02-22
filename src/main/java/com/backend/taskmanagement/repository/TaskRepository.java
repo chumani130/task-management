13e13e9b1f657fa298b974af5e781143f963a35f
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-22 14:47:08 +0200

    Delete TaskRepository.java

--- src/main/java/com/backend/taskmanagement/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.backend.taskmanagement.repository;
-//
-//import com.backend.taskmanagement.model.Task;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface TaskRepository extends JpaRepository<Task, Long> {
-//}
