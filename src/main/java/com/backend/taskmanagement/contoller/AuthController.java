75e505b8a9636b8eb3e0dc0ec85fd0ad258db72f
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-26 13:02:43 +0200

    Update AuthController.java

--- src/main/java/com/backend/taskmanagement/contoller/AuthController.java
+++ src/main/java/com/backend/taskmanagement/contoller/AuthController.java
@@ -1,9 +1,50 @@
 package com.backend.taskmanagement.contoller;
 
+import com.backend.taskmanagement.dto.AuthenticationRequest;
+import com.backend.taskmanagement.repository.UserRepository;
+import com.backend.taskmanagement.utils.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.*;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+
+    private final UserDetailsService userDetailsService;
+
+    private final UserRepository userRepository;
+
+    private final JwtUtil jwtUtil;
+    @PutMapping("/authentication")
+    public void createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest, HttpServletResponse response) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+        }
+        catch (BadCredentialsException e) {
+            throw new BadCredentialsException("incorrect username or password");
+        }
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        Optional<User> optionalUser = userRepository.findFirstByEmail(userDetails.getUsername());
+        final Spring jwt = jwtUtil.generateToken(userDetails.getUsername());
+
+        if (optionalUser.isPresent()) {
+            response.getWriter().write(new JSONObject()
+                    .put("
+        }
+
+    }
 }
