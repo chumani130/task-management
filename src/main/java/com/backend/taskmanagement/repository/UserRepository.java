c1ce9de66dd8383cae0872a3b3124272acbd8950
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-26 12:34:35 +0200

    Create UserRepository.java

---
+++ src/main/java/com/backend/taskmanagement/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.backend.taskmanagement.repository;
+
+import com.backend.taskmanagement.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findFirstByEmail(String email);
+
+}
