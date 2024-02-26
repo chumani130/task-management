c0ebb42fa27938a910aa3ac15134a323b11441f3
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-26 13:41:47 +0200

    Create AuthService.java

---
+++ src/main/java/com/backend/taskmanagement/services/auth/AuthService.java
@@ -0,0 +1,8 @@
+package com.backend.taskmanagement.services.auth;
+
+import com.backend.taskmanagement.dto.SignupRequest;
+import com.backend.taskmanagement.dto.UserDto;
+
+public interface AuthService {
+    UserDto createUser(SignupRequest signupRequest);
+}
