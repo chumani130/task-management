6e451772523d9f6d9192661343b56a8bb971d01d
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-26 13:41:35 +0200

    Create UserDto.java

---
+++ src/main/java/com/backend/taskmanagement/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.backend.taskmanagement.dto;
+
+import com.backend.taskmanagement.enums.UserRole;
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String email;
+    private String name;
+    private UserRole userRole;
+}
