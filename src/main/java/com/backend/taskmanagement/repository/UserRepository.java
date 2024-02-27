a725be9f5325ec4447bb0f6f20c15d1fc20a284e
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-27 12:36:21 +0200

    Update UserRepository.java

--- src/main/java/com/backend/taskmanagement/repository/UserRepository.java
+++ src/main/java/com/backend/taskmanagement/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.backend.taskmanagement.repository;
 
 import com.backend.taskmanagement.entity.User;
+import com.backend.taskmanagement.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findFirstByEmail(String email);
+
+    User findByRole(UserRole userRole);
 
 }
