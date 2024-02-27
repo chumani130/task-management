962a15feb85da7f6effc1df121ced0b070b98508
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-27 12:09:24 +0200

    Update UserRepository.java

--- src/main/java/com/backend/taskmanagement/repository/UserRepository.java
+++ src/main/java/com/backend/taskmanagement/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.backend.taskmanagement.repository;
 
-import com.backend.taskmanagement.model.User;
+import com.backend.taskmanagement.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
