0ccd5f3c1d52f665dc3cda13db76eeb095e8806f
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-29 10:53:21 +0200

    Update AuthServiceImpl.java

--- src/main/java/com/backend/taskmanagement/services/auth/AuthServiceImpl.java
+++ src/main/java/com/backend/taskmanagement/services/auth/AuthServiceImpl.java
@@ -1,53 +1,53 @@
-package com.backend.taskmanagement.services.auth;
-
-import com.backend.taskmanagement.dto.SignupRequest;
-import com.backend.taskmanagement.dto.UserDto;
-import com.backend.taskmanagement.enums.UserRole;
-import com.backend.taskmanagement.entity.User;
-import com.backend.taskmanagement.repository.UserRepository;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuthServiceImpl implements AuthService{
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    public UserDto createUser(SignupRequest signupRequest) {
-        User user = new User();
-
-        user.setEmail(signupRequest.getEmail());
-        user.setName(signupRequest.getName());
-        user.setPassword(new BCryptPasswordEncoder().encode(signupRequest.getPassword()));
-        user.setRole(UserRole.CUSTOMER);
-        User createUser = userRepository.save(user);
-
-        UserDto userDto = new UserDto();
-        userDto.setId(createUser.getId());
-
-        return userDto;
-
-
-    }
-    public Boolean hasUserWithEmail(String email) {
-        return userRepository.findFirstByEmail(email).isPresent();
-    }
-    @PostConstruct
-    public void createAdminAccount() {
-        User adminAccount = userRepository.findByRole(UserRole.ADMIN);
-        if (null == adminAccount) {
-            User user = new User();
-            user.setEmail("admin@gmail.com");
-            user.setName("admin");
-            user.setRole(UserRole.ADMIN);
-            user.setPassword(new BCryptPasswordEncoder().encode("admin"));
-            userRepository.save(user);
-
-
-        }
-    }
-}
+//package com.backend.taskmanagement.services.auth;
+//
+//import com.backend.taskmanagement.dto.SignupRequest;
+//import com.backend.taskmanagement.dto.UserDto;
+//import com.backend.taskmanagement.enums.UserRole;
+//import com.backend.taskmanagement.entity.User;
+//import com.backend.taskmanagement.repository.UserRepository;
+//import jakarta.annotation.PostConstruct;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class AuthServiceImpl implements AuthService{
+//    @Autowired
+//    private UserRepository userRepository;
+//    @Autowired
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+//
+//    public UserDto createUser(SignupRequest signupRequest) {
+//        User user = new User();
+//
+//        user.setEmail(signupRequest.getEmail());
+//        user.setName(signupRequest.getName());
+//        user.setPassword(new BCryptPasswordEncoder().encode(signupRequest.getPassword()));
+//        user.setRole(UserRole.CUSTOMER);
+//        User createUser = userRepository.save(user);
+//
+//        UserDto userDto = new UserDto();
+//        userDto.setId(createUser.getId());
+//
+//        return userDto;
+//
+//
+//    }
+//    public Boolean hasUserWithEmail(String email) {
+//        return userRepository.findFirstByEmail(email).isPresent();
+//    }
+//    @PostConstruct
+//    public void createAdminAccount() {
+//        User adminAccount = userRepository.findByRole(UserRole.ADMIN);
+//        if (null == adminAccount) {
+//            User user = new User();
+//            user.setEmail("admin@gmail.com");
+//            user.setName("admin");
+//            user.setRole(UserRole.ADMIN);
+//            user.setPassword(new BCryptPasswordEncoder().encode("admin"));
+//            userRepository.save(user);
+//
+//
+//        }
+//    }
+//}
