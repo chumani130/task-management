38ab00cffa683e457f6d71cce5d093b48ac79c49
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-02-29 10:53:39 +0200

    Update User.java

--- src/main/java/com/backend/taskmanagement/entity/User.java
+++ src/main/java/com/backend/taskmanagement/entity/User.java
@@ -1,26 +1,26 @@
-package com.backend.taskmanagement.entity;
-
-import com.backend.taskmanagement.enums.UserRole;
-import jakarta.persistence.*;
-import lombok.Data;
-
-
-@Entity
-@Data
-
-@Table(name = "users")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String password;
-    private String email;
-    private String name;
-    private UserRole role;
-    @Lob
-    @Column(columnDefinition = "longblob")
-    private byte[] img;
-
-    public void setName(String name) {
-    }
-}
+//package com.backend.taskmanagement.entity;
+//
+//import com.backend.taskmanagement.enums.UserRole;
+//import jakarta.persistence.*;
+//import lombok.Data;
+//
+//
+//@Entity
+//@Data
+//
+//@Table(name = "users")
+//public class User {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String password;
+//    private String email;
+//    private String name;
+//    private UserRole role;
+//    @Lob
+//    @Column(columnDefinition = "longblob")
+//    private byte[] img;
+//
+//    public void setName(String name) {
+//    }
+//}
