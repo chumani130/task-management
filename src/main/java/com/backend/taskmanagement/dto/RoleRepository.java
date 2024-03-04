67df53afac647d89ec3a1ac79c03437aa1c4d1b1
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-04 15:16:19 +0200

    Create RoleRepository.java

---
+++ src/main/java/com/backend/taskmanagement/dto/RoleRepository.java
@@ -0,0 +1,8 @@
+//package com.backend.taskmanagement.dto;
+//
+//import com.backend.taskmanagement.model.Role;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface RoleRepository extends JpaRepository<Role, Integer> {
+//    Role findByRole(String user);
+//}
