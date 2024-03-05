2141b489986e98ec0084fd50864a9f3b36291007
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-05 14:02:27 +0200

    Delete CustomerRepository.java

--- src/main/java/com/backend/taskmanagement/dao/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.backend.taskmanagement.dao;
-
-import com.backend.taskmanagement.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
