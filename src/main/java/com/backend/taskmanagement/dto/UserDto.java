f2b1e367bae18e70d97b28a30e58b5852a912a54
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-12 14:38:27 +0200

    Update UserDto.java

--- src/main/java/com/backend/taskmanagement/dto/UserDto.java
+++ src/main/java/com/backend/taskmanagement/dto/UserDto.java
@@ -1,12 +1,13 @@
-//package com.backend.taskmanagement.dto;
-//
-//import com.backend.taskmanagement.enums.UserRole;
-//import lombok.Data;
-//
-//@Data
-//public class UserDto {
-//    private Long id;
-//    private String email;
-//    private String name;
-//    private UserRole userRole;
-//}
+package com.backend.taskmanagement.dto;
+
+import com.backend.taskmanagement.enums.UserRole;
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private UserRole userRole;
+}
