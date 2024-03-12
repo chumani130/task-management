b1d9e67b413580693ed052eb3d93e5215b76ffcb
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-12 15:10:14 +0200

    Update AuthService.java

--- src/main/java/com/backend/taskmanagement/services/auth/AuthService.java
+++ src/main/java/com/backend/taskmanagement/services/auth/AuthService.java
@@ -4,7 +4,7 @@ import com.backend.taskmanagement.dto.SignupRequestDTO;
 import com.backend.taskmanagement.dto.UserDto;
 
 public interface AuthService {
-    UserDto signupCustomer(SignupRequestDTO signupRequestDTO)
+    UserDto signupCustomer(SignupRequestDTO signupRequestDTO);
     UserDto signupAdmin(SignupRequestDTO signupRequestDTO);
 
 
