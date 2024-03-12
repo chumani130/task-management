10a79b63e2b280829b528e1637c3f78ad21fce58
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-12 14:37:54 +0200

    Delete SubTaskRepository.java

--- src/main/java/com/backend/taskmanagement/dao/SubTaskRepository.java
+++
@@ -1,6 +0,0 @@
-package com.backend.taskmanagement.dao;
-
-import com.backend.taskmanagement.model.SubTask;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubTaskRepository extends JpaRepository<SubTask, Long> {}
