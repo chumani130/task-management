811c160165a3b9d08006c96ee1e350142e7d16ed
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-12 15:10:28 +0200

    Update UserService.java

--- src/main/java/com/backend/taskmanagement/services/UserService.java
+++ src/main/java/com/backend/taskmanagement/services/UserService.java
@@ -1,16 +1,16 @@
-package com.backend.taskmanagement.services;
-
-import com.backend.taskmanagement.model.User;
-import com.backend.taskmanagement.model.RegisterUser;
-import java.util.List;
-import java.util.Optional;
-
-public interface UserService {
-    User createUser(RegisterUser registerUser);
-    User getUserById(Long id);
-    List<User> getAllUsers();
-    void deleteUserById(Long id);
-    String updateUserById(Long id, RegisterUser registerUser);
-    Optional<User> getUserByEmail(String email);
-    boolean isUserEmailPresent(String email);
-}
+//package com.backend.taskmanagement.services;
+//
+//import com.backend.taskmanagement.model.User;
+//import com.backend.taskmanagement.model.RegisterUser;
+//import java.util.List;
+//import java.util.Optional;
+//
+//public interface UserService {
+//    User createUser(RegisterUser registerUser);
+//    User getUserById(Long id);
+//    List<User> getAllUsers();
+//    void deleteUserById(Long id);
+//    String updateUserById(Long id, RegisterUser registerUser);
+//    Optional<User> getUserByEmail(String email);
+//    boolean isUserEmailPresent(String email);
+//}
