53b6fdca58fbbc13ac75eccb48a7c918cd115307
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-12 15:10:06 +0200

    Update UserDto.java

--- src/main/java/com/backend/taskmanagement/dto/UserDto.java
+++ src/main/java/com/backend/taskmanagement/dto/UserDto.java
@@ -5,9 +5,14 @@ import lombok.Data;
 
 @Data
 public class UserDto {
+
     private Long id;
+
     private String username;
+
     private String email;
+
     private String password;
+
     private UserRole userRole;
 }
