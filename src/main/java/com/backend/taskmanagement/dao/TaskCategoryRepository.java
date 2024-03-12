3e2fd6577a145fe20795ab0b5f759b85343d0fd6
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-12 14:37:58 +0200

    Delete TaskCategoryRepository.java

--- src/main/java/com/backend/taskmanagement/dao/TaskCategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.backend.taskmanagement.dao;
-
-import com.backend.taskmanagement.model.TaskCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskCategoryRepository extends JpaRepository<TaskCategory, Long> {}
