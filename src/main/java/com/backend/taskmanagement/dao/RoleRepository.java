a41919084e2d95f3bb73e3bc31965174d9dbd0a0
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-12 14:37:50 +0200

    Delete RoleRepository.java

--- src/main/java/com/backend/taskmanagement/dao/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.taskmanagement.dao;
-
-import com.backend.taskmanagement.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Role findByRole(String user);
-}
