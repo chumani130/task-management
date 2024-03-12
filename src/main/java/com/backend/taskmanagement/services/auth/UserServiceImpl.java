286b99a1a56f7a1be4038f84a8d2fa5477cb3611
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-12 14:38:53 +0200

    Create UserServiceImpl.java

---
+++ src/main/java/com/backend/taskmanagement/services/auth/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.backend.taskmanagement.services.auth;
+
+import com.backend.taskmanagement.dto.SignupRequestDTO;
+import com.backend.taskmanagement.dto.UserDto;
+import com.backend.taskmanagement.entity.UserEntity;
+import com.backend.taskmanagement.enums.UserRole;
+import com.backend.taskmanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserServiceImpl implements AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDto signupCustomer(SignupRequestDTO signupRequestDTO) {
+        UserEntity user = new UserEntity();
+
+        user.setName(signupRequestDTO.getName());
+        user.setLastname(signupRequestDTO.getLastname());
+        user.setEmail(signupRequestDTO.getEmail());
+        user.setPhone(signupRequestDTO.getPhone());
+        user.setPassword(signupRequestDTO.getPassword());
+
+        user.setRole(UserRole.CUSTOMER);
+
+        return userRepository.save(user).getDto();
+    }
+
+    @Override
+    public UserDto signupAdmin(SignupRequestDTO signupRequestDTO) {
+        UserEntity user = new UserEntity();
+
+        user.setUsername(signupRequestDTO.getUsername();
+        user.setEmail(signupRequestDTO.getEmail());
+        user.setPassword(signupRequestDTO.getPassword());
+
+        user.setRole(UserRole.ADMIN);
+
+        return userRepository.save(user).getDto();
+    }
+
+    @Override
+    public Boolean presentByEmail(String email) {
+        return null;
+    }
+}
