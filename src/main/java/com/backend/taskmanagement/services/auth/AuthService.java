a2998bfa5a6ffa7122de438234e03579d30018c3
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-03-12 14:38:50 +0200

    Update AuthService.java

--- src/main/java/com/backend/taskmanagement/services/auth/AuthService.java
+++ src/main/java/com/backend/taskmanagement/services/auth/AuthService.java
@@ -1,10 +1,12 @@
-//package com.backend.taskmanagement.services.auth;
-//
-//import com.backend.taskmanagement.dto.SignupRequest;
-//import com.backend.taskmanagement.dto.UserDto;
-//
-//public interface AuthService {
-//    UserDto createUser(SignupRequest signupRequest);
-//
-//    Boolean hasUserWithEmail(String email);
-//}
+package com.backend.taskmanagement.services.auth;
+
+import com.backend.taskmanagement.dto.SignupRequestDTO;
+import com.backend.taskmanagement.dto.UserDto;
+
+public interface AuthService {
+    UserDto signupCustomer(SignupRequestDTO signupRequestDTO)
+    UserDto signupAdmin(SignupRequestDTO signupRequestDTO);
+
+
+    Boolean presentByEmail(String email);
+}
